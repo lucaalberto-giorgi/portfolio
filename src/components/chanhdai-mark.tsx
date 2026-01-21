e58f8aa6026ec2d54e18e61e65be633a2fe1338f
Luca Alberto Giorgi <lucaalberto-giorgi@users.noreply.github.com>
2026-01-21 23:15:55 +0000

    Change header initials from CD to LG

--- src/components/chanhdai-mark.tsx
+++ src/components/chanhdai-mark.tsx
@@ -6,14 +6,22 @@ export function ChanhDaiMark(props: React.ComponentProps<"svg">) {
       viewBox="0 0 512 256"
       {...props}
     >
-      <path
+      <text
+        x="256"
+        y="180"
+        fontSize="200"
+        fontFamily="system-ui, -apple-system, sans-serif"
+        fontWeight="600"
         fill="currentColor"
-        d="M192 256H64v-64h128v64ZM448 64H320v128h128v64H256V0h192v64ZM64 192H0V64h64v128ZM512 192h-64V64h64v128ZM192 64H64V0h128v64Z"
-      />
+        textAnchor="middle"
+        dominantBaseline="middle"
+      >
+        LG
+      </text>
     </svg>
   );
 }
 
 export function getMarkSVG(color: string) {
-  return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 256 128"><path fill="${color}" d="M96 128H32V96h64v32ZM224 32h-64v64h64v32h-96V0h96v32ZM32 96H0V32h32v64ZM256 96h-32V32h32v64ZM96 32H32V0h64v32Z"/></svg>`;
+  return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 256 128"><text x="128" y="90" fontSize="100" fontFamily="system-ui, -apple-system, sans-serif" fontWeight="600" fill="${color}" textAnchor="middle" dominantBaseline="middle">LG</text></svg>`;
 }

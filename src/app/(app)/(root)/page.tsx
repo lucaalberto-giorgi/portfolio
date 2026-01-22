1b9189c3ea8f3eaa407e5fe7aa58e438d784ebc2
Luca Alberto Giorgi <lucaalberto-giorgi@users.noreply.github.com>
2026-01-22 00:54:08 +0000

    Add Projects component to homepage

--- src/app/(app)/(root)/page.tsx
+++ src/app/(app)/(root)/page.tsx
@@ -2,6 +2,7 @@ import type { ProfilePage as PageSchema, WithContext } from "schema-dts";
 
 import { About } from "@/features/portfolio/components/about";
 import { ProfileHeader } from "@/features/portfolio/components/profile-header";
+import { Projects } from "@/features/portfolio/components/projects";
 import { Skills } from "@/features/portfolio/components/skills";
 import { SocialLinks } from "@/features/portfolio/components/social-links";
 import { USER } from "@/features/portfolio/data/user";
@@ -25,6 +26,9 @@ export default function Page() {
         <Separator />
 
         <About />
+        <Separator />
+
+        <Projects />
         <Separator />
 
         <Skills />
